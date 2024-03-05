66422eff6d6d78e63171737739d5c2da22b3928d
Vatsalmadhur <bhartimadhur@gmail.com>
2024-03-05 19:04:06 +0530

    .

--- src/app/Navbar/navbar.tsx
+++ src/app/Navbar/navbar.tsx
@@ -1,17 +1,33 @@
 "use client"
 import React from "react";
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button} from "@nextui-org/react";
-// import {AcmeLogo} from "./AcmeLogo.jsx";
 
-export default function NavbarComponent() {
+export default function App() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    "Home",
+    "Events",
+    "Sponsors",
+  ];
+
   return (
-    <Navbar>
-      <NavbarBrand>
-        <p className="font-bold text-inherit">ACME</p>
-      </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+    <Navbar onMenuOpenChange={setIsMenuOpen} style={{border:"2px solid red"}}>
+      <NavbarContent style={{border:"2px solid red",}} className="sm" >
+      <NavbarContent className="w-100" style={{border:"2px solid green"} }>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand >
+          {/* <AcmeLogo /> */}
+          <p className="font-bold text-inherit ">iMMERSE</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className=" sm:flex gap-4" justify="center">
         <NavbarItem>
-          <Link color="foreground" href="#">
+          <Link color="foreground" href="/about">
             Features
           </Link>
         </NavbarItem>
@@ -35,6 +51,23 @@ export default function NavbarComponent() {
             Sign Up
           </Button>
         </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={
+                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
+              }
+              className="w-full"
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
       </NavbarContent>
     </Navbar>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,12 @@ import styles from "./page.module.css";
 import NavbarComponent from "./Navbar/navbar";
 import { Button } from "@nextui-org/react";
 import 'tailwindcss/tailwind.css'
+// import '../globals.css'
 
 export default function Home() {
   return (
     <>
-    <p>hekko</p>
+    <p className="text-3xl">home</p>
     </>
   );
 }
